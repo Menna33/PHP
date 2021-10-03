f67bb2e3ef5deb8d0fc9d85dacfd410f85d251d0
Menna33 <mennalotfy33@gmail.com>
2021-10-03 18:22:52 +0200

    It is a simple calculator

---
+++ calculator.php
@@ -0,0 +1,52 @@
+<?php
+$num1=10;
+$num2=5;
+//to try specific operation comment all other and uncomment it
+//$operation="addition";
+//$operation="subtraction";
+//$operation="multiplication";
+$operation="division";
+//addition
+switch($operation)
+{
+    case "addition":
+        $sum=$num1+$num2;
+        echo "The addition result is: ";
+        echo $sum;
+        break;
+
+    case "subtraction":
+        $sub=$num1-$num2;
+        echo "The subtraction result is: ";
+        echo $sub;
+        break;
+
+    case "multiplication":
+        $res=$num1*$num2;
+        echo "The multiplication result is: ";
+        echo $res;
+        break;
+
+    case "division":
+        $res=$num1/$num2;
+        echo "The division result is: ";
+        echo $res;
+        break;
+    default:
+    break;
+    
+}
+/*$sum=$num1+$num2;
+echo $sum;
+//subtraction
+$sub=$num1-$num2;
+echo $sub;
+//multiplication
+$sub=$num1*$num2;
+echo $sub;
+//division
+$sub=$num1/$num2;
+echo $sub;*/
+
+
+?>
